409ee730889f34ba79a09a49ca432103b9eb0d46
fengyiping <fengyp@shinemo.com>
2016-02-18 10:44:52 +0800

    Initial Commit

---
+++ Guess-Who/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guess-Who
+//
+//  Created by 冯颐平 on 16/2/18.
+//  Copyright © 2016年 冯颐平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Guess-Who/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guess-Who
+//
+//  Created by 冯颐平 on 16/2/18.
+//  Copyright © 2016年 冯颐平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
