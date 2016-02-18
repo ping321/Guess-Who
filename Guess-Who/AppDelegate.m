5e72d8d02c68af87d29825d335b3c58d36d3d9d6
fengyiping <fengyp@shinemo.com>
2016-02-18 18:00:17 +0800

    fix confict

--- Guess-Who/AppDelegate.m
+++ Guess-Who/AppDelegate.m
@@ -20,9 +20,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window =[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    [self.window makeKeyAndVisible];
-    self.window.rootViewController =  [[HomeViewController alloc]init];
+   
     return YES;
 }
 
